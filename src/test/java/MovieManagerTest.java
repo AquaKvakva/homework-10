24403ada3cd73111e684630ac036b35c25272ef2
Olga Sukhareva <3830921@gmail.com>
2025-04-28 13:58:39 +0300

    more tests

--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -124,6 +124,46 @@ public class MovieManagerTest {
 
     }
 
+    @Test
+    public void shouldFindLastOverLimit() {
+        MovieManager manager = new MovieManager(3);
+
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
+        manager.addMovie("Film 6");
+        manager.addMovie("Film 7");
+
+        String[] expected = {"Film 7", "Film 6", "Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldFindLastLimit() {
+        MovieManager manager = new MovieManager(7);
+
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
+        manager.addMovie("Film 6");
+        manager.addMovie("Film 7");
+
+        String[] expected = {"Film 7", "Film 6", "Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+
+
 
 
 
